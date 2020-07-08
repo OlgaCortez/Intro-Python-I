c8db43f0c6ae58d43a307554686a735266919805
Olga Cortez <olgalataviacortez@gmail.com>
2020-07-07 20:04:44 -0500

    finished 10 - 15 except for 14_cal.py, will work on this next

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
 # YOUR CODE HERE
+f = open("foo.txt", "r")
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-
 # YOUR CODE HERE
+fo = open("bar.txt", "w")
+fo.write("2020 so far\n")
+fo.write("has been one big\n")
+fo.write("mess!")
+fo.close()
+
+fo = open("bar.txt", "r")
+print(fo.read())
+fo.close()
